8064dd024b252798e8a7a65b16213582472a2a0a
Nzube Brian <125203580+SirGentle21@users.noreply.github.com>
2023-07-08 23:18:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.post('/', async (req, res) => {
       temperature: 0,
       max_tokens: 4096,
       top_p: 1,
-      frequency_penalty: 1,
+      frequency_penalty: 0.5,
       presence_penalty: 0,
     });
 
